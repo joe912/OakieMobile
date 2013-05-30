c663c23a3c21a91db69b84150e2fefcba0fca0ed
Joe Dillon <joe912@gmail.com>
2013-05-30 23:17:06 +0100

    Initial version

---
+++ OakieMobile/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OakieMobile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "The Oakie....";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About the Oakie...";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact the Oakie...";
+
+            return View();
+        }
+
+        public ActionResult NextMatch()
+        {
+            ViewBag.Message = "Next match.";
+
+            return View();
+        }        
+        
+        public ActionResult Fixtures()
+        {
+            ViewBag.Message = "Fixtures.";
+
+            return View();
+        }
+    }
+}
